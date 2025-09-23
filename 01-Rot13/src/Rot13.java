ee97ece83695ce919c8bc4a1e51f53635ff7944c
alba0403 <2024_alba.melendres@iticbcn.cat>
2025-09-23 19:27:39 +0200

    Activitat 1-Rot13 finalitzada

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,44 +1,91 @@
+/* Programa que conté dues funcions principals, una que xifra cada caràcter de la frase substituint-lo per el que està a 13 posicions
+ * per endavant, i l'altra funció desxifra, substituint el caràcter pel que està a 13 posicions endarrere.
+ */
 public class Rot13 {
     
+    //Arrays a nivell de classe
+    static final char[] arrayLletresMin = "aàábcçdeèéfghiíìïjklmnñoóòpqrstuúùüvwxyz".toCharArray();
+    static final char[] arrayLletresMaj = "AÀÁBCÇDEÈÉFGHIÌÍÏJKLMNÑOÓÒPQRSTUÚÙÜVWXYZ".toCharArray();
     public static void main(String[] args) {
-        String exemple = "Hola";
-       
-        xifraRot13(exemple);
+        String exemple1 = "Hola";
+        String exemple2 = "Òwúi";
 
+        String resultatEncrip = xifraRot13(exemple1);
+        System.out.println("Xifrat :" + resultatEncrip);
+
+        String resultatDesencrip = desxifraRot13(exemple2);
+        System.out.println("Desxifrat :" + resultatDesencrip);
     }
 
-    public static String xifraRot13(String cadena){
-        // creem un array de l'abecedari en miniscules
-        String lletresMinusucules = "aàábcçdeèéfghíìïjklmnñopqrstuvwxyz";
-        char[] arrayLletresMin = lletresMinusucules.toCharArray();
-
-        // creem un array de l'abecedari en majuscules
-        String lletresMajuscules = "AÀÁBCÇDEÉÈFGHIÍÌÏJKLMNÑOPQRSTUVWXYZ";
-        char[] arrayLletresMaj = lletresMajuscules.toCharArray();
-
-        //string que va obtenint els caracters
-        //un cop encriptada la lletra
+    public static String xifraRot13(String paraula){
+        //String que va obtenint els caràcters un cop encriptada la lletra
         String paraulaEncriptada = "";
-
-        //recorregut de la paraula donada
-        for(int i = 0; i < cadena.length(); i++){
+        
+        // recorregut de la paraula hola
+        for(int i = 0; i < paraula.length(); i++){
             // tornem la lletra en caracter
-            char lletra = cadena.charAt(i);
+            char lletra = paraula.charAt(i);
             //si la lletra es majuscula
-            if(Character.isUpperCase(lletra) == true){
-                
-                for
-
-            } else if(){    // si la lletra es minuscula
-                for
-            } else {        // si la lletra no es majuscula ni minuscula
-
+            if(Character.isUpperCase(lletra)){
+                paraulaEncriptada += xifrarLletra(lletra, arrayLletresMaj);
+            } else if(Character.isLowerCase(lletra)){    // si la lletra es minuscula
+                paraulaEncriptada += xifrarLletra(lletra, arrayLletresMin);
+            } else {   // si la lletra no es majuscula ni minuscula
+                paraulaEncriptada += lletra;
             }
         }
         return paraulaEncriptada;
     }
 
-    public static String desxifraRot13(String cadena){
-        return null;
+    public static String desxifraRot13(String paraula){
+        String paraulaDesencriptada = "";
+
+        // recorregut de la paraula Òwúi
+        for(int i = 0; i < paraula.length(); i++){
+            // tornem la lletra en caracter
+            char lletra = paraula.charAt(i);
+            //si la lletra es majuscula
+            if(Character.isUpperCase(lletra)){
+                paraulaDesencriptada += desxifrarLletra(lletra, arrayLletresMaj);
+            } else if(Character.isLowerCase(lletra)){    // si la lletra es minuscula
+                paraulaDesencriptada += desxifrarLletra(lletra, arrayLletresMin);
+            } else {        
+                paraulaDesencriptada += lletra;
+            }
+        }
+        return paraulaDesencriptada;
+    }
+
+    public static char xifrarLletra(char lletra, char[] abecedari){
+        int posicio = 0;
+        for (char c : abecedari){
+            if (lletra == c){
+                posicio = posicio + 13;
+                // si la posicio és més llarga que l'array, es resta la llargada de l'array a la posició
+                if (posicio >= abecedari.length){
+                    posicio -= abecedari.length;                        
+                }
+                return abecedari[posicio];
+            }
+            posicio++;
+        }
+        return lletra;
+    }
+
+    public static char desxifrarLletra(char lletra, char[] abecedari){
+        int posicio = 0;
+        //recorregut de l'abecedari (array)
+        for (char c : abecedari){
+            if (lletra == c){
+                posicio = posicio - 13;
+                // si la posicio és més curta que l'array, es suma la llargada de l'array a la posició
+                if (posicio < 0){
+                    posicio += abecedari.length;
+                }
+                return abecedari[posicio];
+            }
+            posicio++;
+        }
+        return lletra;
     }
 }
